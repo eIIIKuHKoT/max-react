d5da16fc78da87952aeffa5b57b9851885142fc2
Evgeny <evgeny.pochtarenko@softermii.com>
2020-03-06 16:41:42 +0200

    render list

--- react-guide/src/components/Burger/Burger.js
+++ react-guide/src/components/Burger/Burger.js
@@ -4,11 +4,17 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+  const ingredients = Object.keys(props.ingredients)
+    .map(key => {
+      return [...Array(props.ingredients[key])]
+        .map((_, i) => {
+          return <BurgerIngredient key={key+i} type={key} />
+        })
+    });
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>
-      <BurgerIngredient type="cheese"/>
-      <BurgerIngredient type="meat"/>
+      {ingredients}
       <BurgerIngredient type="bread-bottom"/>
     </div>
   )
