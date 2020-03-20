21847634c2c8a6406f84a329d92e7d0674a03819
Evgeny <evgeny.pochtarenko@softermii.com>
2020-03-20 14:49:35 +0200

    SideDrawer

--- react-guide/src/components/Layout/Layout.js
+++ react-guide/src/components/Layout/Layout.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, {Component} from "react";
 
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-  <Auxiliary>
-    <Toolbar/>
-    <SideDrawer/>
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Auxiliary>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  };
+  
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false});
+  };
+  sideDrawerOpenHandler = () => {
+    this.setState((prevStet) => {
+      return {showSideDrawer: !prevStet.showSideDrawer}
+    });
+  };
+  
+  
+  render() {
+    return (
+      <Auxiliary>
+        <Toolbar
+          drawerToggleClicked={this.sideDrawerOpenHandler}
+        />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Auxiliary>
+    );
+  }
+}
 
-export default layout;
+export default Layout;
