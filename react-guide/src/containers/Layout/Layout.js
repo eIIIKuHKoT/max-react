d9a2a4e10db4c295f439f137e04be1d8d7310980
Evgeny <evgeny.pochtarenko@softermii.com>
2020-03-20 16:25:11 +0200

    restructure

--- react-guide/src/containers/Layout/Layout.js
+++ react-guide/src/containers/Layout/Layout.js
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
   state = {
