e7df50b64077a387c75c21dff6f2e97524f4046f
Evgeny <evgeny.pochtarenko@softermii.com>
2020-03-24 09:46:00 +0200

    fetch orders

--- react-guide/src/containers/Orders/Orders.js
+++ react-guide/src/containers/Orders/Orders.js
@@ -1,8 +1,31 @@
 import React, {Component} from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler'
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: false
+  };
+  
+  componentDidMount() {
+    axios.get('/orders.js')
+      .then(res => {
+        let orders = [];
+        for(let key in res.data) {
+          orders.push({
+            ...res.data[key],
+            id: key
+          })
+        }
+        this.setState({orders})
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
   
   render() {
     return (
@@ -14,4 +37,4 @@ class Orders extends Component {
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
