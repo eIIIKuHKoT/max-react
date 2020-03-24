293ac478de049ccb81f5b169d8fd29135b9edf9b
Evgeny <evgeny.pochtarenko@softermii.com>
2020-03-24 15:06:28 +0200

    input component

--- react-guide/src/containers/Checkout/ContactData/ContactData.js
+++ react-guide/src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css'
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
@@ -43,14 +44,15 @@ class ContactData extends Component {
   };
   
   render() {
-    let form = <form>
-      <input type="text" name="name" placeholder="Your name"/>
-      <input type="email" name="email" placeholder="Your email"/>
-      <input type="text" name="street" placeholder="Your address"/>
-      <input type="number" name="postalCode" placeholder="Your Postal Code"/>
-      <Button buttonType="Success" clicked={this.orderHandler}>Order</Button>
-    </form>
-    if(this.state.loading){
+    let form =
+      <form>
+        <Input inputеtype="input" type="text" name="name" placeholder="Your name"/>
+        <Input inputеtype="input" type="email" name="email" placeholder="Your email"/>
+        <Input inputеtype="input" type="text" name="street" placeholder="Your address"/>
+        <Input inputеtype="input" type="text" name="postalCode" placeholder="Your Postal Code"/>
+        <Button buttonType="Success" clicked={this.orderHandler}>Order</Button>
+      </form>;
+    if (this.state.loading) {
       form = <Spinner/>
     }
     return (
