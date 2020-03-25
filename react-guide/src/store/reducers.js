0428b7d9a39577decd46d98afba2bbb2b1635ba5
Evgeny <evgeny.pochtarenko@softermii.com>
2020-03-25 16:07:44 +0200

    setup redux

---
+++ react-guide/src/store/reducers.js
@@ -0,0 +1,13 @@
+import {ADD_INGREDIENT, REMOVE_INGREDIENT} from './actions';
+
+
+const initialState = {
+  ingredients: null,
+  totalPrice: 4
+};
+
+const reducer = (state = initialState, action) => {
+
+};
+
+export default reducer;
