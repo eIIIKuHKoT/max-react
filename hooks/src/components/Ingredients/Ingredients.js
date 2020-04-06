99e4096096d0fa24cc7e519370a83c1b2671d4d0
Evgeny <evgeny.pochtarenko@softermii.com>
2020-04-06 16:03:33 +0300

    save ingredients

--- hooks/src/components/Ingredients/Ingredients.js
+++ hooks/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import IngredientList from "./IngredientList";
 
 function Ingredients() {
+  
+  const [ingredients, setIngredients] = useState([]);
+  
+  const addIngredientHandler = async ingredient => {
+    let response =  await fetch('https://burger-builder-42c71.firebaseio.com/ingredients-hooks.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-type':'application/json'}
+    });
+    response = await response.json();
+    setIngredients(prevIngs => [...prevIngs, {
+      id: response.name,
+      ...ingredient
+    }]);
+  };
+  
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList onRemoveItem={() => {}} ingredients={ingredients}/>
       </section>
     </div>
   );
