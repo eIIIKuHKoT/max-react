9a7326ecade345007d8a857736357d12047a6aa4
Evgeny <evgeny.pochtarenko@softermii.com>
2020-04-10 11:36:44 +0300

    optimize modal

--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Modal.module.css';
-import Auxiliary from '../../../hoc/Auxiliary';
-import Backdrop from '../Backdrop/Backdrop';
+import classes from "./Modal.module.css";
+import Auxiliary from "../../../hoc/Auxiliary";
+import Backdrop from "../Backdrop/Backdrop";
 
 const modal = (props) => (
   <Auxiliary>
-    <Backdrop show={props.show} clicked={props.modalClosed}/>
+    <Backdrop show={props.show} clicked={props.modalClosed} />
     <div
       style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
+        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+        opacity: props.show ? "1" : "0",
       }}
-      className={classes.Modal}>
+      className={classes.Modal}
+    >
       {props.children}
     </div>
   </Auxiliary>
 );
 
-export default modal;
+export default React.memo(
+  modal,
+  (prevProps, nextProps) =>
+    nextProps.show === prevProps.show &&
+    nextProps.children === prevProps.children
+);
